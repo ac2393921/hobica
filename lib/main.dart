ac185fc7c3e0098c89ddaede9ac37171e8c525cf
Masaya Kojima <ac2393921@gmail.com>
2026-02-25 15:13:04 +0900

    takt: github-issue-38-week5-6-fyeezu-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hobica/app.dart';
 import 'package:hobica/core/database/app_database.dart';
 import 'package:hobica/core/database/providers/database_provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   final db = AppDatabase();
+  FlutterNativeSplash.remove();
   runApp(
     ProviderScope(
       overrides: [appDatabaseProvider.overrideWithValue(db)],
